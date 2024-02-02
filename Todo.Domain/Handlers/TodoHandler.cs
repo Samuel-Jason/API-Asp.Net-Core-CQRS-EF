e6035bf302f54f34be3926ae31eae1c54aa60622
Samuel Jason <Samuelljason@gmail.com>
2024-02-01 21:23:43 -0300

    Aplicando e Refatorando Testes unitarios.

--- Todo.Domain/Handlers/TodoHandler.cs
+++ Todo.Domain/Handlers/TodoHandler.cs
@@ -13,12 +13,22 @@ namespace Todo.Domain.Handlers
             _repository = repository
         }
 
-        public ICommandResult Handle (CreateTodoCommand command)
+        public ICommandResult Handle(CreateTodoCommand command)
         {
             //fail fast validation
             command.Validate();
-            if(command.Invalid)
-                Return new GenericCommandResult(false, "Ops erro", command.Notifications);
+
+            if (command.Invalid)
+               Return new GenericCommandResult(false, "Ops erro", command.Notifications);
+
+            //Gera o Todo
+            var todo = new TodoItem(command.Title, command.User, command.Date);
+
+            //Salva no banco
+            _repository.Create(todo);
+
+            //Retorna o Resultado
+            return new GenericCommandResult(true, "Tarefa Salva", command.Date);
         }
     }
 }
--- Todo.Domain/obj/Release/net8.0/Todo.Domain.AssemblyInfo.cs
+++ Todo.Domain/obj/Release/net8.0/Todo.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todo.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d4d6a1c6460365e60f1fdc48be00caf05d9a100")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11bc300585979ea2d6ed5701264b03a000e15920")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todo.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todo.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
